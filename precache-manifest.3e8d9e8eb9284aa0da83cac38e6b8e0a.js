26d9a4774b46d3adf7e4670112f3ff20065cca4f
VladStorozhenko <43917096+VladStorozhenko@users.noreply.github.com>
2020-08-07 14:41:52 +0300

    Updates

---
+++ precache-manifest.3e8d9e8eb9284aa0da83cac38e6b8e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e7c11f7b1a428e58c5053aeb1c529",
+    "url": "/reactrecipeapp/index.html"
+  },
+  {
+    "revision": "717b88910b82c2d5a41e",
+    "url": "/reactrecipeapp/static/css/main.f98d6793.chunk.css"
+  },
+  {
+    "revision": "e6607ab21eaf90bf52fd",
+    "url": "/reactrecipeapp/static/js/2.b51a9c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactrecipeapp/static/js/2.b51a9c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717b88910b82c2d5a41e",
+    "url": "/reactrecipeapp/static/js/main.8ce2a843.chunk.js"
+  },
+  {
+    "revision": "484d2372febdf62836eb",
+    "url": "/reactrecipeapp/static/js/runtime-main.62c440ef.js"
+  }
+]);
